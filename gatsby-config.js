8bb7937396ee4d7ab7871af8f691a8b88a5ada5d
Sung Kim <sungmkim80@gmail.com>
2019-01-20 21:50:20 -0500

    Fixed site meta data for SEO

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Hi, I am Sung 👋`,
+    description: `Sung M. Kim's Home Page`,
+    author: `Sung M. Kim`,
   },
   plugins: [
     `gatsby-plugin-sass`,
@@ -34,14 +34,6 @@ module.exports = {
         exclude: ['/preview/**', '/do-not-track/me/too/'],
       },
     },
-    // {
-    //   resolve: 'gatsby-plugin-react-svg',
-    //   options: {
-    //     rule: {
-    //       include: /img/,
-    //     },
-    //   },
-    // },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
